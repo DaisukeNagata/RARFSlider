e9949dc693bad20f0e5051f9792651bcf93ea424
永田大祐 <dbank0208@gmail.com>
2019-09-19 01:17:30 +0900

    change optinal AVPlayer

--- RARFSlider/Classes/RARFModel/RARFAVPlayerModel.swift
+++ RARFSlider/Classes/RARFModel/RARFAVPlayerModel.swift
@@ -12,7 +12,7 @@ import AVFoundation
 public final class RARFAVPlayerModel {
 
     var url: URL?
-    var videoPlayer: AVPlayer!
+    var videoPlayer: AVPlayer?
     var playerItem: AVPlayerItem?
 
 
@@ -23,11 +23,11 @@ public final class RARFAVPlayerModel {
         videoPlayer = AVPlayer(playerItem: playerItem)
     }
 
-    func currentTime() -> Float64 { return CMTimeGetSeconds(videoPlayer.currentItem?.currentTime() ?? CMTime()) }
+    func currentTime() -> Float64 { return CMTimeGetSeconds(videoPlayer?.currentItem?.currentTime() ?? CMTime()) }
 
-    func videoDurationTime() -> Float64 { return CMTimeGetSeconds(videoPlayer.currentItem?.duration ?? CMTime()) }
+    func videoDurationTime() -> Float64 { return CMTimeGetSeconds(videoPlayer?.currentItem?.duration ?? CMTime()) }
 
-    func videoSeek(change: Float) { videoPlayer.seek(to:CMTimeMakeWithSeconds(Float64(change), preferredTimescale: Int32(NSEC_PER_SEC))) }
+    func videoSeek(change: Float) { videoPlayer?.seek(to:CMTimeMakeWithSeconds(Float64(change), preferredTimescale: Int32(NSEC_PER_SEC))) }
 
     func videoImageViews(nowTime: Float64) -> UIImage {
         let interval = CMTime(seconds: nowTime, preferredTimescale: CMTimeScale(Int32(NSEC_PER_SEC)))
