222bf66f594e133ab16200679f5b31540005df86
永田大祐 <dbank0208@gmail.com>
2019-09-21 13:24:45 +0900

    add struct RARFStructure

---
+++ RARFStructure/RARFStructure.swift
@@ -0,0 +1,15 @@
+//
+//  RARFStructure.swift
+//  Nimble
+//
+//  Created by 永田大祐 on 2019/09/21.
+//
+
+import UIKit
+
+struct RARFStructure {
+    // Gesche to determine image selection range
+    static var swipePanGesture = UIPanGestureRecognizer()
+    // VC for bind
+    static var callBacks = { () -> Void in }
+}
